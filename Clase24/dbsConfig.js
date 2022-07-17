bd6067ab2cc42c014d4ae9a2a416b30f086d580a
Brenda Bernardi <brendabernardi99@gmail.com>
2022-07-17 16:59:26 -0300

    desafio24

---
+++ Clase24/dbsConfig.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+export const db = mongoose.connect("mongodb+srv://brendabernardi22:<password>@cluster0.uduv7.mongodb.net/?retryWrites=true&w=majority", 
+{ useNewUrlParser: true })
+
+const chatSchema = new mongoose.Schema({
+    author: {type: Object, required: true },
+    text: {type: String, required: true},
+    time: {type: String, required: true}
+}, {
+    versionKey: false 
+})
+
+export const msgsModel = mongoose.model("Msgs", chatSchema);
+
+
+const productSchema = new mongoose.Schema({
+    name: {type: String, required: true, max: 25},
+    price: {type: Number, required: true},
+    stock: {type: Number, required: true},
+    photo: {type: String, required: true},
+    code: {type: String, required: true, max: 10},
+    desc: {type: String, required: true, max: 100},
+})
+
+export const productsModel = mongoose.model("Products", productSchema);
